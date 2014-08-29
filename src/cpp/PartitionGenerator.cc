470f2e28880a171a5f2d0dedf5bf074a57dd41c1
Matthieu DC <decamym@gmail.com>
2014-08-29 19:10:35 +0200

    C++ class added

--- src/cpp/PartitionGenerator.cc
+++ src/cpp/PartitionGenerator.cc
@@ -1,7 +1,9 @@
 #include "PartitionGenerator.h"
+#include <stdlib.h>
+#include <iostream>
 
 PartitionGenerator::PartitionGenerator()
-	:recording(false)
+	:recording(false), nbNoiseIterations(30)
 {
     
 }
@@ -9,5 +11,67 @@ PartitionGenerator::PartitionGenerator()
 void 
 PartitionGenerator::setRecording(bool recording)
 {
-    this.recording : recording;
+	if (recording && !this->recording)
+	{
+		this->cancelNoise(this->nbFreqs);
+	}
+
+    this->recording = recording;
+}
+
+void
+PartitionGenerator::detectNote(uint8_t* fft, size_t len, float frame_size)
+{
+	this->nbFreqs = len;
+	//add to noise table
+	if (!recording)
+	{
+		std::cout << "caca" << std::endl;
+		this->noise.push(fft);
+		if (this->noise.size() > this->nbNoiseIterations)
+		{
+			this->noise.pop();
+		}
+	}
+	//Process fft
+	else
+	{
+		std::cout << "pipi" << std::endl;
+		frame_size = 0; //-Wall
+	}
+}
+
+void
+PartitionGenerator::cancelNoise(size_t len)
+{
+	this->finalNoise = (uint8_t *)malloc(len * sizeof(uint8_t));
+	int nbIters = this->noise.size();
+
+	for (size_t i = 0; i < this->nbFreqs; ++i)
+	{
+		this->finalNoise[i] = 0;		
+	}
+
+	while(!this->noise.empty())
+	{
+		uint8_t *noiseIter = this->noise.back();
+		
+		for (size_t i = 0; i < this->nbFreqs; ++i)
+		{
+			this->finalNoise[i] += noiseIter[i];
+		}
+
+		this->noise.pop();
+	}
+
+	for (size_t i = 0; i < len; ++i)
+	{
+		this->finalNoise[i] /= nbIters;
+	}
+}
+
+PartitionGenerator::~PartitionGenerator()
+{
+    free(this->finalNoise);
+    this->finalNoise = 0;
 }
--- src/cpp/PartitionGenerator.h
+++ src/cpp/PartitionGenerator.h
@@ -1,15 +1,41 @@
 #ifndef PARTITION_GENERATOR_HH
 #define PARTITION_GENERATOR_HH
 
+#include <stdint.h>
+#include <stdio.h>
+#include <queue>
+#include <emscripten/bind.h>
+
+using namespace emscripten;
+
 class PartitionGenerator
 {
 public:
-	void setRecording(bool recording);
+	static PartitionGenerator& getInstance()
+	{
+		static PartitionGenerator instance;
 
+		return instance;
+	}
+
+	~PartitionGenerator();
+	void setRecording(bool recording);
+	void detectNote(uint8_t* fft, size_t len, float frame_size);
 
 private:
-	bool recording;
+	PartitionGenerator();
+	PartitionGenerator(PartitionGenerator const&);
+    void operator=(PartitionGenerator const&);
+	
+	//Methods
+	void cancelNoise(size_t len);
 
+	bool recording;
+	//We keep noise for the last 30 iterations (~3s)
+	std::queue<uint8_t*> noise;
+	uint8_t* finalNoise;
+	size_t nbNoiseIterations;
+	size_t nbFreqs;
 };
 
 #endif /* PARTITION_GENERATOR_HH */
