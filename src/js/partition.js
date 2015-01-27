d18cdb67691cd583eaf03773e991dacd46e0e254
Matthieu DC <decamym@gmail.com>
2015-01-27 17:44:41 +0100

    Removed first note that had nothing to do there

--- src/js/partition.js
+++ src/js/partition.js
@@ -1,7 +1,6 @@
 window.score = new JellyScore.ScoreInterface();
 window.score.resetScore();
 window.score.draw($('article'));
-window.score.addNote(0, 65, 7)
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -9,6 +8,7 @@ function getRandomInt(min, max) {
 
 var i = 0;
 function customAddNote() {
+  
   var j = getRandomInt(1, 12);
   window.score.addNote(i, getRandomInt(50,80), j);
   i += j;
